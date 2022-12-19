af45ab8a18db3d078c48e1902dd1c5f38f8cf5da
unkn3wn <reyesfernando151@gmail.com>
2022-12-19 12:49:06 -0800

    almost done

--- src/components/AllCrypto.jsx
+++ src/components/AllCrypto.jsx
@@ -17,8 +17,10 @@ export default function AllCrpyto() {
 
   return (
     <div >
-      <div>
+      <h1>SEARCH FOR A CRYPTO OF YOUR CHOICE</h1>
+      <div >
         <input
+          className="bg-red-300 text-center w-full"
           type="text"
           placeholder="search"
           onChange={(event) => {
@@ -40,28 +42,47 @@ export default function AllCrpyto() {
           })
           .map((info) => {
             return (
-              <div className={styles.format}>
-                <div className={styles.cards}>
+              <div key={info.id} className={styles.format}>
+                <div  className={styles.cards}>
                   <div className={styles.name}>
                     {" "}
-                    <h1 className={styles.sym}>SYM: {info.symbol.toUpperCase()}</h1>
+                    <h1 className={styles.sym}>
+                      SYM: {info.symbol.toUpperCase()}
+                    </h1>
                     <h1>{info.name}</h1>
                   </div>
-
                   {info.price_change_percentage_24h < 0 ? (
                     <p className="text-red-600 text-center">
                       {info.price_change_percentage_24h.toFixed(2)}%
                     </p>
                   ) : (
                     <p className="text-green-400 text-center">
-                      {info.price_change_percentage_24h.toFixed(2)}%
+                      {info.price_change_percentage_24h}%
                     </p>
                   )}
+
                   <img className={styles.image} src={info.image} />
-                  <h1 className={styles.wordingLeft}> <span className={styles.darkerText}>PRICE: </span> ${info.current_price}</h1>
-                  <h1 className={styles.wordingLeft}> <span className={styles.darkerText}>MARKET CAP 24HRS: </span> {info.market_cap}</h1>
-                  <h1 className={styles.wordingLeft}> <span className={styles.darkerText}>VOLUME: </span> {info.total_volume}</h1>
-                  <h6 className={styles.smaller}><span className={styles.darkerText}>LAST UPDATED: </span>{info.last_updated}</h6>
+                  <h1 className={styles.wordingLeft}>
+                    {" "}
+                    <span className={styles.darkerText}>PRICE: </span> $
+                    {info.current_price}
+                  </h1>
+                  <h1 className={styles.wordingLeft}>
+                    {" "}
+                    <span className={styles.darkerText}>
+                      MARKET CAP 24HRS:{" "}
+                    </span>{" "}
+                    {info.market_cap}
+                  </h1>
+                  <h1 className={styles.wordingLeft}>
+                    {" "}
+                    <span className={styles.darkerText}>VOLUME: </span>{" "}
+                    {info.total_volume}
+                  </h1>
+                  <h6 className={styles.smaller}>
+                    <span className={styles.darkerText}>LAST UPDATED: </span>
+                    {info.last_updated}
+                  </h6>
                 </div>
               </div>
             );
